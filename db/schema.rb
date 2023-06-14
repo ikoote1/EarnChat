7c55e1fae541b9ce152f93dd8cf23ba7d88bb4fe
ikoote1 <ikootepreim@gmail.com>
2023-06-14 16:32:21 +0300

    earnchat

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_132422) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_133036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "users_id", null: false
+    t.bigint "posts_id", null: false
+    t.text "text", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.index ["users_id"], name: "index_comments_on_users_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "users_id", null: false
@@ -34,5 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_132422) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts", column: "posts_id"
+  add_foreign_key "comments", "users", column: "users_id"
   add_foreign_key "posts", "users", column: "users_id"
 end
