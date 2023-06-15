aaa9c1bc6b319ec92ae99a4aca1b981e8bdb51a1
ikoote1 <ikootepreim@gmail.com>
2023-06-15 15:40:22 +0300

    modeling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,17 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :posts
-    has_many :likes
+  belongs_to :user
+  has_many :posts
+  has_many :likes
 
-    def recent_comments(limit = 5)
-        comments.order(created_at: :desc).limit(limit)
-      end
-    
-      def update_comments_counter
-        update(comments_counter: comments.count)
-      end
-    
-      def update_likes_counter
-        update(likes_counter: likes.count)
-      end
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
+
+  def update_comments_counter
+    update(comments_counter: comments.count)
+  end
+
+  def update_likes_counter
+    update(likes_counter: likes.count)
+  end
 end
