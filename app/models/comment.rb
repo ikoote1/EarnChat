ba26c7de7c75b1480d5b91c1d6d796c1d578a91e
ikoote1 <ikootepreim@gmail.com>
2023-06-15 17:59:52 +0300

    correction

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_create :update_post_comments_counter
-  
+
   def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
   end
