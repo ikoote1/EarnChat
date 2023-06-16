89e750fdfd1f7c3e424d04f21ea7789dc331808a
ikoote1 <ikootepreim@gmail.com>
2023-06-16 11:46:38 +0300

    Adding unit tests and validations to the models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :posts, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 
+  validate :title, presence: true, length: {maximum: 200}
+  validate :comments_counter, comparison: {greater_than_or_equal_to: :0}, numericality: {only_integer: true}
+  validate :likes_counter, comparison: {greater_than_or_equal_to: :0}, numericality: {only_integer: true}
+
   def update_user_posts_counter
     user.update(posts_counter: user.posts.count)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
 
+  validate :name, presence: true
+  validate :posts_counter, comparison: {greater_than_or_equal_to: :0}, numericality: {only_integer: true}
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
