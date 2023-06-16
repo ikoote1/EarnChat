bcf4e7391a03d1579b49b9da535451d2f50c7b30
ikoote1 <ikootepreim@gmail.com>
2023-06-16 19:07:18 +0300

    fixing the requirements

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   let(:user) { User.create(name: 'Ikoote', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I always loved to work with you in my life', posts_counter: 5) }
-  let(:post) { Post.create(user: user, title: 'My name is God', text: 'I always loved to work with you in my life', comments_counter: 0, likes_counter: 0) }
-  let!(:comment) { Comment.create(user: user, post: post, text: 'This is a comment') }
+  let(:post) do
+    Post.create(user:, title: 'My name is God', text: 'I always loved to work with you in my life', comments_counter: 0,
+                likes_counter: 0)
+  end
+  let!(:comment) { Comment.create(user:, post:, text: 'This is a comment') }
 
   before do
     post.update(comments_counter: 0)
@@ -11,9 +14,9 @@ RSpec.describe Comment, type: :model do
 
   describe '#update_post_comments_counter' do
     it 'updates the comments_counter of the associated post' do
-      expect {
+      expect do
         comment.update_post_comments_counter
-      }.to change { post.reload.comments_counter }.from(0).to(1)
+      end.to change { post.reload.comments_counter }.from(0).to(1)
     end
   end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   let(:user) { User.create(name: 'Ikoote', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I always loved to work with you in my life', posts_counter: 5) }
-  let(:post) { Post.create(user: user, title: 'My name is God', text: 'I always loved to work with you in my life', comments_counter: 20, likes_counter: 0) }
-  let!(:like) { Like.create(user: user, post: post) }
+  let(:post) do
+    Post.create(user:, title: 'My name is God', text: 'I always loved to work with you in my life',
+                comments_counter: 20, likes_counter: 0)
+  end
+  let!(:like) { Like.create(user:, post:) }
 
   before do
     post.update(likes_counter: 0)
@@ -11,9 +14,9 @@ RSpec.describe Like, type: :model do
 
   describe '#update_post_likes_counter' do
     it 'updates the likes_counter of the associated post' do
-      expect {
+      expect do
         like.update_post_likes_counter
-      }.to change { post.reload.likes_counter }.from(0).to(1)
+      end.to change { post.reload.likes_counter }.from(0).to(1)
     end
   end
 end
