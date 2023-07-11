1c7816913ab2ebcc31df20bc74a2ab4f4c3da540
ikoote1 <ikootepreim@gmail.com>
2023-07-11 14:30:06 +0300

    adding controllers and routes to the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # get 'users', controller: 'users', action: 'index'
-  root 'users#index'
+  get 'users', to: 'users#index'
   get 'users/:id', to: 'users#show', as: 'user'
   get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
