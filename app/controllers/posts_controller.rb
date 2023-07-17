fd30678452b01729b44edf8842100379580841ac
ikoote1 <ikootepreim@gmail.com>
2023-07-17 17:45:09 +0300

    working on routes and controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,8 @@
 class PostsController < ApplicationController
-    def index
-        @user = User.find(params[:user_id])
-        @posts = @user.posts
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
-    def show
-    end
+  def show; end
 end
