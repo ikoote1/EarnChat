f38042018787e30801867833818d6a383ba23bb3
ikoote1 <ikootepreim@gmail.com>
2023-07-21 00:40:05 +0300

    forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   get 'comments/create'
 
-  # get 'users', controller: 'users', action: 'index'
-  # get 'users', to: 'users#index'
-  # get 'users/:id', to: 'users#show', as: 'user'
-  # get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  # get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
     end
@@ -16,9 +11,4 @@ Rails.application.routes.draw do
       get 'show_to_index' # This will generate the route helper method user_show_to_index_path
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # resources :posts
 end
