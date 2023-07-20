e7eb0b8a286c158f6e049ef1e10fc3570eafa6a1
ikoote1 <ikootepreim@gmail.com>
2023-07-20 11:54:59 +0300

    working on project view and front end

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts.includes(:comments) # Fetch the comments associated with each post
   end
 
-  def show; end
+  def show
+  end
 end
