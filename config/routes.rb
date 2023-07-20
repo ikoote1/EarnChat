d553413f55ea023ee11417143a8a02b66bb405dc
ikoote1 <ikootepreim@gmail.com>
2023-07-20 18:21:35 +0300

    implementing forms in the application

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   # get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   # get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
+    
     member do
       get 'show_to_index' # This will generate the route helper method user_show_to_index_path
     end
