de2fac1a9e88d477159f3e98722335c3eaaf06c8
ikoote1 <ikootepreim@gmail.com>
2023-07-20 18:35:41 +0300

    implementing forms in the application

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
-end
+    def create
+      @user = User.find(params[:user_id])
+      @post = @user.posts.find(params[:post_id])
+      @like = @post.likes.build(user: @user)
+  
+      if @like.save
+        redirect_to user_post_path(@user, @post), notice: 'Like added.'
+      else
+        redirect_to user_post_path(@user, @post), alert: 'Error adding like.'
+      end
+    end
+  end
+  
