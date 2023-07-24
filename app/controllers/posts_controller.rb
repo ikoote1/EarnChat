ba968fa321156f69b108e0a8eac075a4f23a50f4
ikoote1 <ikootepreim@gmail.com>
2023-07-24 16:55:41 +0300

    implementing specs and removing n+1 problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,8 @@ class PostsController < ApplicationController
     @post = @user.posts.build
   end
 
-  def show
-    @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:id])
-  end
+  # def show
+  #   @user = User.find(params[:user_id])
+  #   @post = @user.posts.find(params[:id])
+  # end
 end
