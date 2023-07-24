8383a3d5defad9969ece9bdf0592c042bfa2e1d2
ikoote1 <ikootepreim@gmail.com>
2023-07-25 01:55:37 +0300

    implementing specs for views

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.feature 'User Show Page' do
   scenario 'displays user details and redirects to user post index page on click' do
-    # Assuming you have some users and posts in your test database
     user = User.create(name: 'Ikoote rasuli', photo: 'user_photo.jpg', bio: 'I am good', posts_counter: 3)
     post1 = Post.create(user: user, title: 'Post 1', text: 'Post 1 text')
     post2 = Post.create(user: user, title: 'Post 2', text: 'Post 2 text')
@@ -16,20 +15,18 @@ RSpec.feature 'User Show Page' do
     expect(page).to have_content('Number of posts: 3')
     expect(page).to have_content('I am good')
 
+    # Check if each post is displayed on the page
     expect(page).to have_content('Post 1')
     expect(page).to have_content('Post 2')
     expect(page).to have_content('Post 3')
 
+    # Ensure the "See all posts" button is present
+    expect(page).to have_button('See all posts')
+
+    # Click the "See all posts" button
     click_button 'See all posts'
-    expect(current_path).to eq(show_to_index_user_path(user))
+
+    # After clicking the button, check the redirection
+    expect(current_path).to eq(user_posts_path(user))
   end
-
-  # scenario 'redirects to post show page on click post title' do
-  #   user = User.create(name: 'Ikoote rasuli', photo: 'user_photo.jpg', bio: 'I am good', posts_counter: 5)
-  #   post = Post.create(user: user, title: 'Post Title', text: 'Post text')
-
-  #   visit user_path(user)
-  #   click_link 'Post Title'
-  #   expect(current_path).to eq(user_post_path(user, post))
-  # end
 end
