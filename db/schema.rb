ecddee66957f7411ec6b82a81bf45f40d8c5146b
ikoote1 <ikootepreim@gmail.com>
2023-07-26 12:25:52 +0300

    working on authentications

--- db/schema.rb
+++ db/schema.rb
@@ -10,52 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_133312) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_26_085050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # create_table "comments", force: :cascade do |t|
-  #   t.bigint "user_id", null: false
-  #   t.bigint "post_id", null: false
-  #   t.text "text", null: false
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.index ["post_id"], name: "index_comments_on_post_id"
-  #   t.index ["user_id"], name: "index_comments_on_user_id"
-  # end
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.text "text", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
-  # create_table "likes", force: :cascade do |t|
-  #   t.bigint "user_id", null: false
-  #   t.bigint "post_id", null: false
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.index ["post_id"], name: "index_likes_on_post_id"
-  #   t.index ["user_id"], name: "index_likes_on_user_id"
-  # end
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
-  # create_table "posts", force: :cascade do |t|
-  #   t.bigint "user_id", null: false
-  #   t.string "title", null: false
-  #   t.text "text", null: false
-  #   t.integer "comments_counter", default: 0
-  #   t.integer "likes_counter", default: 0
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.index ["user_id"], name: "index_posts_on_user_id"
-  # end
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.text "text", null: false
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
-  # create_table "users", force: :cascade do |t|
-  #   t.string "name", null: false
-  #   t.string "photo", null: false
-  #   t.string "bio", null: false
-  #   t.integer "posts_counter", default: 0
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "photo", null: false
+    t.string "bio", null: false
+    t.integer "posts_counter", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
-  # add_foreign_key "comments", "posts"
-  # add_foreign_key "comments", "users"
-  # add_foreign_key "likes", "posts"
-  # add_foreign_key "likes", "users"
-  # add_foreign_key "posts", "users"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
