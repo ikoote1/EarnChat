6aad9fc8f9a8be40caf60b886b4a33df8479891c
ikoote1 <ikootepreim@gmail.com>
2023-07-28 02:54:30 +0300

    adding authorization

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource :post
+  load_and_authorize_resource :comment, through: :post, shallow: true
+
   def create
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:post_id])
@@ -12,9 +15,36 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment.destroy
+    redirect_to user_post_path(@user, @post), notice: 'Comment was successfully deleted.'
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:text)
   end
 end
+
+
+# class CommentsController < ApplicationController
+#   def create
+#     @user = User.find(params[:user_id])
+#     @post = @user.posts.find(params[:post_id])
+#     @comment = @post.comments.build(comment_params)
+#     @comment.user = @user
+
+#     if @comment.save
+#       redirect_to user_post_path(@user, @post), notice: 'Comment was successfully created.'
+#     else
+#       redirect_to user_post_path(@user, @post), alert: 'Error creating comment.'
+#     end
+#   end
+
+#   private
+
+#   def comment_params
+#     params.require(:comment).permit(:text)
+#   end
+# end
