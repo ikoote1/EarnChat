2da0d08185099d2cc5e9aae7ffc9d30f95805d21
ikoote1 <ikootepreim@gmail.com>
2023-07-28 01:56:43 +0300

    adding authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
@@ -9,12 +10,13 @@ class PostsController < ApplicationController
     @post = @user.posts.build
   end
 
-  # def show
-  #   @user = User.find(params[:user_id])
-  #   @post = @user.posts.find(params[:id])
-  # end
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.includes(:comments).find(params[:id])
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to user_posts_path(current_user), notice: 'Post was successfully deleted'
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.role == 'admin'
+      can :destroy, Post
+    else
+      can :destroy, Post, author_id: user.id
+    end
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
