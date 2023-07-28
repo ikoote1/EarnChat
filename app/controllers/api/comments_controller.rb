ac4de4e184811ab2fba2715664973b336200bf10
ikoote1 <ikootepreim@gmail.com>
2023-07-28 16:57:04 +0300

    creating APIs for comments, posts and users

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,32 +1,29 @@
-# controllers/api/comments_controller.rb
-#http://localhost:3000/api/users/1/posts/1/comments
 module Api
-    class CommentsController < ApiController
-      def index
-        @user = User.find(params[:user_id])
-        @post = @user.posts.find(params[:post_id])
-        @comments = @post.comments
-        render json: @comments
-      end
-  
-      def create
-        @user = User.find(params[:user_id])
-        @post = @user.posts.find(params[:post_id])
-        @comment = @post.comments.build(comment_params)
-        @comment.user = @user
-  
-        if @comment.save
-          render json: @comment, status: :created
-        else
-          render json: { error: 'Error creating comment.' }, status: :unprocessable_entity
-        end
-      end
-  
-      private
-  
-      def comment_params
-        params.require(:comment).permit(:text)
+  class CommentsController < ApiController
+    def index
+      @user = User.find(params[:user_id])
+      @post = @user.posts.find(params[:post_id])
+      @comments = @post.comments
+      render json: @comments
+    end
+
+    def create
+      @user = User.find(params[:user_id])
+      @post = @user.posts.find(params[:post_id])
+      @comment = @post.comments.build(comment_params)
+      @comment.user = @user
+
+      if @comment.save
+        render json: @comment, status: :created
+      else
+        render json: { error: 'Error creating comment.' }, status: :unprocessable_entity
       end
     end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
   end
-  
+end
--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,12 +1,9 @@
-# controllers/api/comments_controller.rb
-#http://localhost:3000/api/users/1/posts/1/comments
 module Api
-    class PostsController < ApiController
-      def index
-        @user = User.find(params[:user_id])
-        @post = @user.posts.
-        render json: @posts
-      end
+  class PostsController < ApiController
+    def index
+      @user = User.find(params[:user_id])
+      @posts = @user.posts # Add this line to fetch the user's posts
+      render json: @posts
     end
   end
-  
+end
