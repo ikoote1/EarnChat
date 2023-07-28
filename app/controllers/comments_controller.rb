7a9b73a155685b9559bc65a13a011cf95a3b54fc
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-28 13:05:16 +0530

    Updates comment_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,24 +26,3 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:text)
   end
 end
-
-# class CommentsController < ApplicationController
-#   def create
-#     @user = User.find(params[:user_id])
-#     @post = @user.posts.find(params[:post_id])
-#     @comment = @post.comments.build(comment_params)
-#     @comment.user = @user
-
-#     if @comment.save
-#       redirect_to user_post_path(@user, @post), notice: 'Comment was successfully created.'
-#     else
-#       redirect_to user_post_path(@user, @post), alert: 'Error creating comment.'
-#     end
-#   end
-
-#   private
-
-#   def comment_params
-#     params.require(:comment).permit(:text)
-#   end
-# end
